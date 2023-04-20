8d6b7d2c0b0902805b39b4a0eb51294347d88145
Your Name <you@example.com>
2023-04-20 17:41:10 -0300

    Update

---
+++ app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AboutPage(){
+return (
+  <div>
+    <h1>About Page</h1>
+    <p>lorem ipsum</p>
+  </div>
+)
+
+
+}
+
+export default AboutPage 
---
+++ app/services/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ServicePage(){
+return (
+  <div>
+    <h1>Services</h1>
+  </div>
+)
+
+
+}
+
+export default ServicePage 
