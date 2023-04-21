156f80969cb1f707c6f4be5a3e8301cc65db7e96
Your Name <you@example.com>
2023-04-21 13:34:13 -0300

    a

--- app/recipebook/RecipeBook.jsx
+++ app/recipebook/RecipeBook.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 export function RecipeBook() {
   return (
     <div>
-      
+      <h1>Recetario</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At voluptatibus velit libero officiis ipsa, ducimus sit! Mollitia adipisci ullam consequatur ab tenetur. Odio quasi laborum consectetur sed numquam? Nemo ut sint praesentium, nostrum dolorum earum ducimus. Fugiat laborum, explicabo, ad veniam quos iusto inventore praesentium illum molestiae eos voluptatibus nihil.</p>
     </div>
   );
 }
