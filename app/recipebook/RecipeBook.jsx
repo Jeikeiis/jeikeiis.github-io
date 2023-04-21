fa8ff327e6975fea2fb52c9a0bef1eb7e33126bc
Your Name <you@example.com>
2023-04-21 14:43:19 -0300

    a

--- app/recipebook/RecipeBook.jsx
+++ app/recipebook/RecipeBook.jsx
@@ -1,6 +1,12 @@
 export function RecipeBook() {
   return (
     <div>
+      <div class="grid">
+        <div class="header"><br>
+                <div class="introheader"><h1>Nutrición</h1></div>
+				<img src="("https://images.pexels.com/photos/1640770/pexels-photo-1640770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+        </div>
+    </div>
     </div>
   );
 
