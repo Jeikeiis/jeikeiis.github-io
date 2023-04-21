15a00144d80e2ea199378a685a07d1498e7b794a
Your Name <you@example.com>
2023-04-20 21:20:47 -0300

    update

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,21 +1,17 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 function Navigator() {
   return (
-    <>
-
-    <h1>Navigation</h1>
     <ul>
-          <li>
-            <Link href="/" >Inicio</Link>
-          </li>
-          <li>
-          <Link href="/about" >Sobre nosotros</Link>
-          </li>
-          <li>
-           <Link href="/services" >Recetario</Link>
-          </li>
-        </ul>
-</>  
+      <li>
+        <Link href="/">Inicio</Link>
+      </li>
+      <li>
+        <Link href="/about">Sobre nosotros</Link>
+      </li>
+      <li>
+        <Link href="/services">Recetario</Link>
+      </li>
+    </ul>
   );
 }
