9bde99794c7f6bb55da183643f20d56591f84e3f
Your Name <you@example.com>
2023-04-21 14:55:18 -0300

    a

--- app/recipebook/RecipeBook.jsx
+++ app/recipebook/RecipeBook.jsx
@@ -1,12 +1,10 @@
+import React from 'react';
+
 export function RecipeBook() {
   return (
     <div>
-      <div class="grid">
-        <div class="header"><br>
-                <div class="introheader"><h1>Nutrición</h1></div>
-				<img src="("https://images.pexels.com/photos/1640770/pexels-photo-1640770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-        </div>
-    </div>
+      <h1>Recetario</h1><br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero rem obcaecati perferendis quos assumenda sapiente est praesentium, exercitationem, ullam ipsa fugiat voluptatum, culpa neque? Cumque quod illo voluptate, quo aperiam, quaerat impedit similique temporibus quam atque sint. Minima dolorum similique repudiandae excepturi quibusdam quis, optio sint quod, officia ratione rem facere, maxime quae ea quidem beatae repellendus maiores fugiat at fuga accusantium recusandae. Facere dicta natus debitis. Debitis eaque modi corrupti voluptatibus illum, sint cum suscipit asperiores! In eum ducimus rerum perferendis, dolorem ea quidem deserunt repellat cumque voluptates sequi ad illo, deleniti sed earum qui consequatur aliquam molestiae illum.</p>
     </div>
   );
-
+}
